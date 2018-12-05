51ac59e1e7a84d65dc0aaf73b2cb538e1493bf00
Furkan Türkal <furkan.turkal@hotmail.com>
2018-12-06 00:22:49 +0300

    added install.sh template

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "
+ ██████╗██╗   ██╗ ██████╗██╗     ███████╗███╗   ██╗███████╗
+██╔════╝╚██╗ ██╔╝██╔════╝██║     ██╔════╝████╗  ██║██╔════╝
+██║      ╚████╔╝ ██║     ██║     █████╗  ██╔██╗ ██║███████╗
+██║       ╚██╔╝  ██║     ██║     ██╔══╝  ██║╚██╗██║╚════██║
+╚██████╗   ██║   ╚██████╗███████╗███████╗██║ ╚████║███████║
+ ╚═════╝   ╚═╝    ╚═════╝╚══════╝╚══════╝╚═╝  ╚═══╝╚══════╝
+                                                           
+Install Script
+"
+
+install_debian_package(){
+  # Package install codes...
+}
+
+install_debian(){ 
+  # Codes...
+}
+
+install_arch_package(){
+  # Package install codes...
+}
+
+install_arch(){
+  # Codes...
+}
+
+setup(){
+  # Setup after install codes...
+}
